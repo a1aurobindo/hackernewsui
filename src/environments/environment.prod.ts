0ee0ab046fc19874ee43820b46a79099c811cb7e
a1aurobindo <aurobindo.parida59@gmail.com>
2023-06-21 23:18:59 +0530

    MF3 Detail: removed cognito related dependencies

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,3 @@
 export const environment = {
   production: true,
-  cognitoUserPoolId: 'us-east-1_hG68GYSbu',
-  cognitoAppClientId: '44cd7irogaten9t2oio871d7rp',
-  hostIP: "192.168.1.4",
-  port: 8080,
-  protocol: 'http://'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,6 @@
 
 export const environment = {
   production: false,
-  cognitoUserPoolId: 'us-east-1_hG68GYSbu',
-  cognitoAppClientId: '44cd7irogaten9t2oio871d7rp',
-  hostIP: "192.168.1.4",
-  port: 8080,
-  protocol: 'http://'
 };
 
 /*
